aac42bb8a895bb3fc9dd27d5bb924816918903f6
wx172296059 <wangxuan@ytx.com>
2018-06-25 17:42:28 +0800

    snowflake

---
+++ src/main/java/com/nero/NeroSnowflakeDemoApplication.java
@@ -0,0 +1,12 @@
+package com.nero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeroSnowflakeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeroSnowflakeDemoApplication.class, args);
+	}
+}
